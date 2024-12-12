c4fbfabb97270537c51f000190317f642bf60279
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2024-12-12 09:32:49 +0100

    retirer bug padding des partenaires/ section à propos + problème padding/margin du heade/bandeau

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,1 @@
 
-const carousel = document.querySelector('.old-edition__carousel');
-carousel.addEventListener('wheel', (e) => {
-    e.preventDefault();
-    carousel.scrollBy({
-        left: e.deltaY < 0 ? -100 : 100,
-        behavior: 'smooth',
-    });
-});
