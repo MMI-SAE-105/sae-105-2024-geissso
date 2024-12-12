5bcf26b34a8383b15ff4f3883028e4442a60aa51
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2024-12-12 14:36:05 +0100

    je galere avec mon menu.....

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.querySelector(".menu-btn");
+    const nav = document.querySelector("#mainNav");
 
+   
+    nav.setAttribute("aria-hidden", "true");
+    toggle.setAttribute("aria-expanded", "false");
+
+    toggle.addEventListener("click", (evt) => {
+        const isOpen = toggle.getAttribute("aria-expanded") === "true";
+        const isClosed = !isOpen;
+
+        nav.setAttribute("aria-hidden", isOpen ? "true" : "false");
+        toggle.setAttribute("aria-expanded", isClosed.toString());
+    });
+});
