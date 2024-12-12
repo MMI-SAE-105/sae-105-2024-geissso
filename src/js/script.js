40c7000d59984b1987bdd965998637038dfd11f8
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2024-12-12 22:35:42 +0100

    relier les pages html au menu + ENFIN UN MENU JS POTABLE mais la presentation est encore à revoir

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.querySelector(".menu-btn");
-    const nav = document.querySelector("#mainNav");
+    const toggle = document.querySelector(".menu-btn"); // Bouton burger
+    const nav = document.querySelector(".menu"); // Menu
+    
+    toggle.addEventListener("click", () => {
+        const isOpen = toggle.getAttribute("aria-expanded") === "true"; // Vérifier si le menu est ouvert
+        const isClosed = !isOpen; // Inverser l'état du menu
 
-   
-    nav.setAttribute("aria-hidden", "true");
-    toggle.setAttribute("aria-expanded", "false");
+        console.log("isOpen : ", isOpen, "isClosed :", isClosed); // Log pour vérifier
 
-    toggle.addEventListener("click", (evt) => {
-        const isOpen = toggle.getAttribute("aria-expanded") === "true";
-        const isClosed = !isOpen;
-
-        nav.setAttribute("aria-hidden", isOpen ? "true" : "false");
-        toggle.setAttribute("aria-expanded", isClosed.toString());
+        // Modifiez l'état du menu et du bouton
+        nav.setAttribute("aria-hidden", isOpen ? "true" : "false"); // Afficher/masquer le menu
+        toggle.setAttribute("aria-expanded", isClosed.toString()); // Modifier l'état du bouton
     });
+});
+document.querySelector('.back-button').addEventListener('click', () => {
+    window.history.back();
 });
