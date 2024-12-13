75fcabefeed63d083b6e297e1a7808b2c4b27af4
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2024-12-13 11:16:52 +0100

    remise en place du css du footer et le menu ne marchait car bug javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => { // Correction ici
+    // Gestion du menu burger
+    const toggle = document.querySelector(".menu-btn"); // Bouton burger
+    const nav = document.querySelector(".menu"); // Menu
+
+    if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+            const isOpen = toggle.getAttribute("aria-expanded") === "true"; // Vérifier si le menu est ouvert
+            const isClosed = !isOpen; // Inverser l'état du menu
+
+            // Modifier l'état du menu et du bouton
+            nav.setAttribute("aria-hidden", isOpen ? "true" : "false"); // Afficher/masquer le menu
+            toggle.setAttribute("aria-expanded", isClosed.toString()); // Modifier l'état du bouton
+        });
+    }
+});
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const carouselContainer = document.querySelector(".carousel__container");
     const items = document.querySelectorAll(".carousel__item");
